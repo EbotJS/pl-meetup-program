b7f71f8caf974ccc5ddc2131f785d7e5bccf1405
EbotJS <achaleebotoma2002@gmail.com>
2021-10-16 08:33:38 +0100

    pl-meetup-program

---
+++ mine7.js
@@ -0,0 +1,12 @@
+//class
+
+class Person {
+    constructor(name,age, height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+}
+
+const Ebot = new Person('Ebot',19,1.5);
+console.log(Ebot.name);
